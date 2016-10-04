5c600ae7e783a994c03fbeafdb4b42ddf20da7be
Dax1n <568275045@qq.com>
2016-10-04 14:57:41 +0800

    添加说明

---
+++ 动态规划代码/src/com/daixn/Investment.java
@@ -0,0 +1,19 @@
+package com.daixn;
+
+/**
+ * 
+ * 动态规划
+ *                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    +
+ * @author 青春常驻Dax1n
+ *
+ * 投资问题：
+ * 		m元钱，n项投资，求投资方案使得收益最大
+ * 
+ * 建模：
+ * 向量 <x1,x2,...xn>，其中xi是标示xi元钱投资给第i个项目
+ *
+ *
+ */
+public class Investment {
+
+}
--- 正则表达式/src/com/daxin/NoneCapture.java
+++ 正则表达式/src/com/daxin/NoneCapture.java
@@ -6,23 +6,43 @@ import java.util.regex.Pattern;
 public class NoneCapture {
 
 	// 非捕获
-	
-	public static void main(String[] args) {
-		
-		Pattern p1=Pattern.compile("(?=a).{3}");//(?=a)写在前面是捕获
 
-		
-		Pattern p=Pattern.compile(".{3}(?=a)");
-		String s="abca456";//三个字符而且三个字符后面是一个a。这样的字符提取出来，但是不把最后一个a提出出来
+	public static void main(String[] args) {
+
+		Pattern p1 = Pattern.compile("(?=a).{3}");// (?=a)写在前面是捕获
+
+		Pattern p = Pattern.compile(".{3}(?=a)");
+		String s = "abca456";// 三个字符而且三个字符后面是一个a。这样的字符提取出来，但是不把最后一个a提出出来
 
 		Matcher m = p.matcher(s);
-		
-		if(m.find()){
+
+		if (m.find()) {
 			System.out.println(m.group());
-		}else{
+		} else {
 			System.out.println("not found...");
 		}
-		
-		
+
+		System.out.println(" 向前引用 :");
+
+		backRefenrence();
+
 	}
+
+	/**
+	 * 向前引用
+	 * 
+	 */
+	public static void backRefenrence() {
+
+		// 组代表：(\\d\\d) 两个数字， \\1表示后面的和前面的两个数字相同的匹配出来
+		//后面的数字1表示向前引用和第几组的相同，1就表示和第一组相同
+		Pattern p = Pattern.compile("(\\d\\d)\\1");
+
+		String s = "1212";// 12后面还是12和前面两个数字12匹配，所以成功
+		Matcher m = p.matcher(s);
+
+		System.out.println(m.matches());
+
+	}
+
 }
